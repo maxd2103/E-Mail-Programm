031ba1e692666727f02d1726c9fc7f0b60056b2a
matttttttttthiias <mat.ruhsert.21@lehre.mosbach.dhbw.de>
2022-04-13 09:42:05 +0200

    Script ausgelagert

---
+++ script.js
@@ -0,0 +1,26 @@
+const  form = document.querySelector('.contact_form');
+//function to send the message when click on button send
+function sendMsg(e){
+    e.preventDefault();
+ 
+        const name = document.querySelector('.animateTransform'),
+            email = document.querySelector('.email'),
+            msg = document.querySelector('.msg');
+
+//8BA998CE5243F82CB8770DCC2F757E5602EB   smtp.elasticemail.com   Port: 2525
+//E-Mail Senden Funktion:
+
+    Email.send({
+   
+    SecureToken : "d79a094d-0b62-483d-842c-fcdbc94e4e12",
+    To : email.value,
+    From : "matthiasruhsert@gmail.com",
+    Subject : "Test",
+    Body : msg.value
+}).then(
+message => alert(message)
+);
+    }
+    
+//add the event listener submit
+form.addEventListener('submit', sendMsg);
