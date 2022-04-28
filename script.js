7d1146ee6bd47c455865e1a6a5fe4fcec4d2069a
gloria.tamakloe <gloria.tamakloe@bechtle.com>
2022-04-28 18:04:18 +0200

    Betreff Zeile und Merhrfachversendung

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const  form = document.querySelector('.contact_form');
 //function to send the message when click on button send
-function sendMsg(e){
-    e.preventDefault();
- 
-        const name = document.querySelector('.animateTransform'),
-            email = document.querySelector('.email'),
-            msg = document.querySelector('.msg');
 
-//8BA998CE5243F82CB8770DCC2F757E5602EB   smtp.elasticemail.com   Port: 2525
-//E-Mail Senden Funktion:
+function sendMsg(email){
 
-    Email.send({
-   
+  const name = document.querySelector('.animateTransform'),
+    msg = document.querySelector('.msg');
+
+  //8BA998CE5243F82CB8770DCC2F757E5602EB   smtp.elasticemail.com   Port: 2525
+  //E-Mail Senden Funktion:
+
+  Email.send({
     SecureToken : "d79a094d-0b62-483d-842c-fcdbc94e4e12",
-    To : email.value,
-    From : "matthiasruhsert@gmail.com",
+    To : email,
+    From : "grusskartenanwendung@gmail.com",
     Subject : "Test",
     Body : msg.value
-}).then(
-message => alert(message)
-);
-    }
-    
+  })
+}
+
+
+function sendMultiMail(e) {
+  e.preventDefault();
+  let email = document.querySelector('.email').value.split(",");
+  email.forEach(sendMsg);
+  alert(message);
+}
+
 //add the event listener submit
-form.addEventListener('submit', sendMsg);
+form.addEventListener('submit', sendMultiMail);
