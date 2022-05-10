d0f47396404077302eb6c3eee46f342a65860c01
Maximilian <dotzler.maximilian@gmail.com>
2022-05-11 00:39:55 +0200

    bug fix fail no xlsx or xls defind

--- JS/gruppen-mail.js
+++ JS/gruppen-mail.js
@@ -1,16 +1,67 @@
 
 type="text/javascript"
 src="https://cdnjs.cloudfare.com/ajax/libs/xlsx/0.15.3/xlsx.full.min.js"
+src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.8.0/jszip.js%22%3E"
+src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.8.0/xlsx.js%22%3E"
 
 var selectedFile;
-document
-.getElementById('fileUpload')
-.addEventListener("change", function(event) {selectedFile = event.target.files[0]
+
+    // UI-Events erst registrieren wenn das DOM bereit ist!
+document.addEventListener("DOMContentLoaded", function() {
+    // Falls neue Eingabe, neuer Aufruf der Auswahlfunktion
+    document.getElementById('fileUpload').addEventListener('change', dateiauswahl, false);
 });
+
+  function dateiauswahl(evt) {
+    // FileList-Objekt des input-Elements auslesen, auf dem 
+    // das change-Event ausgelöst wurde (event.target)
+    selectedFile = evt.target.files[0];
+  }
+
 document.getElementById('uploadExcel')
 .addEventListener('click',function() {
-    if(selectedFile){
-        console.log("hi");
+   
+    var sFilename = selectedFile.name;
+    // Create A File Reader HTML5
+    var reader = new FileReader();
+
+    // Ready The Event For When A File Gets Selected
+    reader.onload = function(e) {
+        var data = e.target.result;
+        var cfb = XLS.CFB.read(data, {type: 'binary'});
+        var wb = XLS.parse_xlscfb(cfb);
+        // Loop Over Each Sheet
+        wb.SheetNames.forEach(function(sheetName) {
+            // Obtain The Current Row As CSV
+            var sCSV = XLS.utils.make_csv(wb.Sheets[sheetName]);
+            var oJS = XLS.utils.sheet_to_row_object_array(wb.Sheets[sheetName]);
+
+            $("#my_file_output").html(sCSV);
+            console.log(oJS)
+        });
+    };
+
+    // Tell JS To Start Reading The File.. You could delay this if desired
+    reader.readAsBinaryString(selectedFile);
+
+
+    /* const XLSX = require('xlsx') ;
+
+    const parseExcel = (selectedFile) => {
+    
+        const excelData = XLSX.readFile(selectedFile);
+    
+        return Object.keys(excelData.Sheets).map(name => ({
+            name,
+            data: XLSX.utils.sheet_to_json(excelData.Sheets[name]),
+        }));
+    };
+    
+    parseExcel("./ss.xls").forEach(element => {
+        console.log(element.data);
+    }) */
+
+       /*  console.log("hi");
         var fileReader = new FileReader();
         fileReader.onload = function(event){
             var data = event.target.result;
@@ -26,7 +77,6 @@ document.getElementById('uploadExcel')
                 console.log(jsonObject);
             });
         };
-        fileReader.readAsBinaryString(selectedFile);
-    }
+        fileReader.readAsBinaryString(selectedFile); */
+    
     });
-
