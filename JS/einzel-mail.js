1df2e2de9e25fdfd0a90f9f28a5db4b2c1e11b90
gloria.tamakloe <gloria.tamakloe@bechtle.com>
2022-05-11 20:33:15 +0200

    image src geändert

--- JS/einzel-mail.js
+++ JS/einzel-mail.js
@@ -6,7 +6,21 @@ function sendMsg(email){
   const name = document.querySelector('.animateTransform'),
     btr = document.querySelector ('.btr'),
     msg = document.querySelector('.msg');
+    const gewaehlteVorlage = getVorlage()
 
+    if (gewaehlteVorlage = "ostern" ){
+      var img = document.createElement("img");
+      img.src = "../src/ostern.png";
+      msg.appendChild(img);
+      window.location.reload();
+      //block.setAttribute("style", "text-align:center");
+  
+    }
+    
+    if (gewaehlteVorlage = "weihn" ){
+      load("vorlageWeihnachten.html"); 
+  
+    }
   //8BA998CE5243F82CB8770DCC2F757E5602EB   smtp.elasticemail.com   Port: 2525
   //E-Mail Senden Funktion:
 
@@ -15,7 +29,7 @@ function sendMsg(email){
     To : email,
     From : "grusskartenanwendung@gmail.com",
     Subject : btr.value,
-    Body : msg.value
+    Body : msg.value,
   }).then(
     message => alert(message)
     );
@@ -34,3 +48,29 @@ function sendMultiMail(e) {
 
 //add the event listener submit
 form.addEventListener('submit', sendMultiMail);
+
+function getVorlage() {
+  const selectElement = document.querySelector('.box-6');
+  const  vorlage= selectElement.options[selectElement.selectedIndex].value;
+  return vorlage;
+}
+
+function includeVorlage(){
+
+  const gewaehlteVorlage = getVorlage()
+
+  if (gewaehlteVorlage = ostern ){
+    var img = document.createElement("img");
+    img.src = "../src/ostern.png";
+    msg.appendChild(img);
+    //block.setAttribute("style", "text-align:center");
+
+  }
+  
+  if (gewaehlteVorlage = weihn ){
+    load("vorlageWeihnachten.html"); 
+
+  }
+
+
+}
