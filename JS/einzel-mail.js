b97ccab82a6c6fb2899adf6529c4bca01b291d6a
Maximilian <dotzler.maximilian@gmail.com>
2022-05-12 11:17:56 +0200

    Vorlagen eingebunden

--- JS/einzel-mail.js
+++ JS/einzel-mail.js
@@ -4,11 +4,78 @@ const  form = document.querySelector('.contact_form');
 function sendMsg(email){
 
   const name = document.querySelector('.animateTransform'),
-    btr = document.querySelector ('.btr'),
-    msg = document.querySelector('.msg');
-    const gewaehlteVorlage = getVorlage()
+    btr = document.querySelector ('.btr')
+  //const gewaehlteVorlage = getVorlage()
 
-    if (gewaehlteVorlage = "ostern" ){
+    var option = ["none", "ostern", "weihn", "bday"];
+    
+    var UserOption = document.getElementById('UserSelect').value;
+    option[UserOption-1]
+
+    if (option[UserOption-1] == "ostern"){
+      msg = `
+              <body>
+                  <img src="../src/ostern.png" style="position: relative;width:950px">
+                      <p style="
+                          position: absolute;
+                          top: 200;
+                          left: 250;
+                          width: 600px;
+                          color: #455621;
+                          font-family: Roboto;
+                          line-height: 1.5;
+                          text-align: justify;">
+                          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                      </p>
+                  </img>
+              </body>
+            `
+      
+    } else if (option[UserOption-1] == "weihn"){
+      msg = `
+              <body>
+                <img src="../src/weihnachten_2.png" style="position: relative;width:950px">
+                    <p style="
+                        position: absolute;
+                        top: 200;
+                        left: 250;
+                        width: 600px;
+                        color: #455621;
+                        font-family: Roboto;
+                        line-height: 1.5;
+                        text-align: justify;">
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    </p>
+                </img>
+              </body>
+            `
+      
+    } else if (option[UserOption-1] == "bday"){
+      msg = `
+              <body>
+                  <img src="src/Bday_1.png" style="position: relative;width:950px">
+                      <p style="
+                          position: absolute;
+                          top: 200;
+                          left: 250;
+                          width: 600px;
+                          color: #455621;
+                          font-family: Roboto;
+                          line-height: 1.5;
+                          text-align: justify;">
+                          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                      </p>
+                  </img>
+              </body>
+            ` 
+      
+    }else if (option[UserOption-1] == "none"){
+      msg = document.querySelector('.msg');
+      
+    }
+
+
+    /* if (gewaehlteVorlage = "ostern" ){
       var img = document.createElement("img");
       img.src = "../src/ostern.png";
       msg.appendChild(img);
@@ -20,7 +87,7 @@ function sendMsg(email){
     if (gewaehlteVorlage = "weihn" ){
       load("vorlageWeihnachten.html"); 
   
-    }
+    } */
   //8BA998CE5243F82CB8770DCC2F757E5602EB   smtp.elasticemail.com   Port: 2525
   //E-Mail Senden Funktion:
 
@@ -33,7 +100,6 @@ function sendMsg(email){
   }).then(
     message => alert(message)
     );
-    clear();
 }
 
 
@@ -49,7 +115,7 @@ function sendMultiMail(e) {
 //add the event listener submit
 form.addEventListener('submit', sendMultiMail);
 
-function getVorlage() {
+/* function getVorlage() {
   const selectElement = document.querySelector('.box-6');
   const  vorlage= selectElement.options[selectElement.selectedIndex].value;
   return vorlage;
@@ -74,3 +140,4 @@ function includeVorlage(){
 
 
 }
+ */
