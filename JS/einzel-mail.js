4736ee9524b78ecf7417ca067f13298db5a3038f
Maximilian <dotzler.maximilian@gmail.com>
2022-05-12 11:47:05 +0200

    test

--- JS/einzel-mail.js
+++ JS/einzel-mail.js
@@ -5,7 +5,7 @@ const  form = document.querySelector('.contact_form');
 function sendMsg(email){
 
   const name = document.querySelector('.animateTransform'),
-    btr = document.querySelector ('.btr')
+    btr = document.querySelector ('.btr');
   //const gewaehlteVorlage = getVorlage()
 
     var option = ["none", "ostern", "weihn", "bday"];
@@ -14,7 +14,7 @@ function sendMsg(email){
     option[UserOption-1]
 
     if (option[UserOption-1] == "ostern"){
-      msg = `
+      var msg = `
               <body>
                   <img src="../src/ostern.png" style="position: relative;width:950px">
                       <p style="
@@ -33,7 +33,7 @@ function sendMsg(email){
             `
       
     } else if (option[UserOption-1] == "weihn"){
-      msg = `
+      var msg = `
               <body>
                 <img src="../src/weihnachten_2.png" style="position: relative;width:950px">
                     <p style="
@@ -52,7 +52,7 @@ function sendMsg(email){
             `
       
     } else if (option[UserOption-1] == "bday"){
-      msg = `
+      var msg = `
               <body>
                   <img src="src/Bday_1.png" style="position: relative;width:950px">
                       <p style="
@@ -71,26 +71,8 @@ function sendMsg(email){
             ` 
       
     }else if (option[UserOption-1] == "none"){
-      msg = document.querySelector('.msg');
-      
+      var msg = document.querySelector('.msg');
     }
-
-
-    /* if (gewaehlteVorlage = "ostern" ){
-      var img = document.createElement("img");
-      img.src = "../src/ostern.png";
-      msg.appendChild(img);
-      window.location.reload();
-      //block.setAttribute("style", "text-align:center");
-  
-    }
-    
-    if (gewaehlteVorlage = "weihn" ){
-      load("vorlageWeihnachten.html"); 
-  
-    } */
-  //8BA998CE5243F82CB8770DCC2F757E5602EB   smtp.elasticemail.com   Port: 2525
-  //E-Mail Senden Funktion:
 
   Email.send({
     SecureToken : "d79a094d-0b62-483d-842c-fcdbc94e4e12",
@@ -116,29 +98,3 @@ function sendMultiMail(e) {
 //add the event listener submit
 form.addEventListener('submit', sendMultiMail);
 
-/* function getVorlage() {
-  const selectElement = document.querySelector('.box-6');
-  const  vorlage= selectElement.options[selectElement.selectedIndex].value;
-  return vorlage;
-}
-
-function includeVorlage(){
-
-  const gewaehlteVorlage = getVorlage()
-
-  if (gewaehlteVorlage = ostern ){
-    var img = document.createElement("img");
-    img.src = "../src/ostern.png";
-    msg.appendChild(img);
-    //block.setAttribute("style", "text-align:center");
-
-  }
-  
-  if (gewaehlteVorlage = weihn ){
-    load("vorlageWeihnachten.html"); 
-
-  }
-
-
-}
- */
