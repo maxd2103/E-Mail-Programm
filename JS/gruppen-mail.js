4fa972be9da2d799e5408f6a5f55de5277c12a7a
Maximilian <dotzler.maximilian@gmail.com>
2022-05-12 13:08:17 +0200

    xlsx auslesen

--- JS/gruppen-mail.js
+++ JS/gruppen-mail.js
@@ -20,46 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.getElementById('uploadExcel')
 .addEventListener('click',function() {
-   
-/*     var sFilename = selectedFile.name;
-    // Create A File Reader HTML5
-    var reader = new FileReader();
-
-    // Ready The Event For When A File Gets Selected
-    reader.onload = function(e) {
-        var data = e.target.result;
-        var cfb = XLS.CFB.read(data, {type: 'binary'});
-        var wb = XLS.parse_xlscfb(cfb);
-        // Loop Over Each Sheet
-        wb.SheetNames.forEach(function(sheetName) {
-            // Obtain The Current Row As CSV
-            var sCSV = XLS.utils.make_csv(wb.Sheets[sheetName]);
-            var oJS = XLS.utils.sheet_to_row_object_array(wb.Sheets[sheetName]);
-
-            $("#my_file_output").html(sCSV);
-            console.log(oJS)
-        });
-    };
-
-    // Tell JS To Start Reading The File.. You could delay this if desired
-    reader.readAsBinaryString(selectedFile); */
-
-
-   /*  const XLSX = require('xlsx') ;
-
-    const parseExcel = (selectedFile) => {
-    
-        const excelData = XLSX.readFile(selectedFile);
-    
-        return Object.keys(excelData.Sheets).map(name => ({
-            name,
-            data: XLSX.utils.sheet_to_json(excelData.Sheets[name]),
-        }));
-    };
-    
-    parseExcel("./ss.xls").forEach(element => {
-        console.log(element.data);
-    }) */
 
          console.log("hi");
         var fileReader = new FileReader();
@@ -69,12 +29,13 @@ document.getElementById('uploadExcel')
                 type: "binary"
             });
             workbook.SheetNames.forEach(sheet => {
-                let rowObject = XLSX.utils.sheet_to_row_onject_array(
-                    workbook.Sheets[sheets]
+                let rowObject = XLSX.utils.sheet_to_row_object_array(
+                    workbook.Sheets[sheet]
                 );
                 let jsonObject = JSON.stringify(rowObject);
-                document.getElementById("jsonData").innerHTML = jsonObject;
+                //document.getElementById("jsonData").innerHTML = jsonObject;
                 console.log(jsonObject);
+                console.log(rowObject);
             });
         };
         fileReader.readAsBinaryString(selectedFile); 
